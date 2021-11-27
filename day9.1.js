9187273b357e6185851ea6651cddce4ecf7b4524
mmp17 <mirzaevmuzaffar@yahoo.com>
2021-11-27 12:48:10 +0900

    js reduce

--- day9.1.js
+++ day9.1.js
@@ -11,14 +11,16 @@ var companies = [
 {"name":"Bauch-McCullough","price":8.15,"category":"Wall Protection","quantity":1},
 {"name":"Gerlach and Sons","price":3.76,"category":"Elevator","quantity":6}]
 
-//foor loop
+//foor loop. for loop repeats until a specified condition evaluates to false. for statement creates a loop that consists of 
+//three optional expressions, enclosed in parentheses and separated by semicolons
+
 // let totalSum = 0;
 // for (let i=0; i<companies.length; i++) {
 //     totalSum = totalSum + (companies[i].price * companies[i].quantity);
 // }
 //console.log(totalSum); //323.03
 
-//forEach
+//forEach. The forEach() method executes a provided function once for each array element.
 
 // let totalForEach = 0;
 // companies.forEach(i => {
@@ -26,3 +28,21 @@ var companies = [
 // })
 
 // console.log(totalForEach);//323.03
+
+//Reduce. The reduce() method executes a reducer function for each value of an array. 
+//reduce() returns a single value which is the function's accumulated result.
+
+// let reducer = companies.reduce((total, i, arr) => {
+//     total += (i.price * i.quantity);
+//     return total
+// },0);
+
+// console.log(reducer); //323.03
+
+// const reducerFn = (value) => {
+//     return companies.reduce((a, value) => {
+//         return a += value.price * value.quantity
+//     }, 0)
+// }
+
+// console.log(reducerFn())//323.03
---
+++ day9.2.js
@@ -0,0 +1,79 @@
+
+var food = [
+    {
+        "name": "McKenzie, Stroman and DuBuque",
+        "price": 6.7,
+        "category": "breakfast",
+        "quantity": 10
+    },
+    {
+        "name": "Leffler and Sons",
+        "price": 4.39,
+        "category": "lunch",
+        "quantity": 4
+    },
+    {
+        "name": "Okuneva-Moore",
+        "price": 2.16,
+        "category": "dinner",
+        "quantity": 4
+    },
+    {
+        "name": "Crooks-Rath",
+        "price": 3.34,
+        "category": "lunch",
+        "quantity": 10
+    },
+    {
+        "name": "Reynolds, O'Hara and Jacobs",
+        "price": 3.65,
+        "category": "breakfast",
+        "quantity": 6
+    },
+    {
+        "name": "Gerlach Group",
+        "price": 9.3,
+        "category": "dinner",
+        "quantity": 9
+    },
+    {
+        "name": "Little-Goodwin",
+        "price": 2.02,
+        "category": "breakfast",
+        "quantity": 1
+    },
+    {
+        "name": "McDermott Inc",
+        "price": 8.3,
+        "category": "lunch",
+        "quantity": 7
+    },
+    {
+        "name": "Bauch-McCullough",
+        "price": 8.15,
+        "category": "dinner",
+        "quantity": 1
+    },
+    {
+        "name": "Gerlach and Sons",
+        "price": 3.76,
+        "category": "lunch",
+        "quantity": 6
+    }
+]
+
+let reducer = food.reduce((result, item) => {
+    const {category, price, quantity} = item;
+    const totalItemPrice = price * quantity;
+    if(item.category === 'lunch') {
+        result.lunch += totalItemPrice
+    } else if (category === 'dinner') {
+        result.dinner += totalItemPrice
+    } else {
+        result.breakfast += totalItemPrice
+    }
+
+    return result;
+},{breakfast: 0, lunch: 0, dinner: 0})
+
+console.log(reducer);
